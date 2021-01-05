e1300fbdac0c7c5c0b69a7e20d90320857240ea1
TeriMHopkins <TeriMHopkins@users.noreply.github.com>
2021-01-04 17:28:53 -0800

    new user inputs

--- Program.cs
+++ Program.cs
@@ -12,17 +12,29 @@ namespace mathAssignment
             Console.WriteLine(" times 50 equals ");
             Console.WriteLine(Convert.ToInt64(UserInput) * 50);
             Console.ReadLine();
+
+            Console.WriteLine("Enter a number: ");
+            string UserInput2 = Console.ReadLine();
             Console.WriteLine(" plus 25 equals ");
-            Console.WriteLine(Convert.ToInt64(UserInput) + 25);
+            Console.WriteLine(Convert.ToInt64(UserInput2) + 25);
             Console.ReadLine();
+
+            Console.WriteLine("Enter a number: ");
+            string UserInput3 = Console.ReadLine();
             Console.WriteLine(" divided by 12.5 equals ");
-            Console.WriteLine(Convert.ToDouble(UserInput) / 12.5);
+            Console.WriteLine(Convert.ToDouble(UserInput3) / 12.5);
             Console.ReadLine();
-            Console.WriteLine(" is greater than 25? ");
-            Console.WriteLine(Convert.ToInt64(UserInput) > 25);
+
+            Console.WriteLine("Enter a number: ");
+            string UserInput4 = Console.ReadLine();
+            Console.WriteLine(" is greater than 50? ");
+            Console.WriteLine(Convert.ToInt64(UserInput4) > 50);
             Console.ReadLine();
+
+            Console.WriteLine("Enter a number: ");
+            string UserInput5 = Console.ReadLine();
             Console.WriteLine(" divided by 7 has a remainder of ");
-            Console.WriteLine(Convert.ToInt64(UserInput) %7);
+            Console.WriteLine(Convert.ToInt64(UserInput5) %7);
             Console.ReadLine();
 
 
